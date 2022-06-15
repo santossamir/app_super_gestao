08b4af5e451f59a641157aeaf6e9c5ad54115e58
Samir Santos <santossamir@hotmail.com>
2022-06-15 20:52:35 -0300

    Aprimorando a estilização das views.

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,7 +7,18 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        $fornecedores = ['Fornecedor 1'];
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'CNPJ' => '00.000.000/000-00'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'N',
+                'CNPJ' => ''
+            ]
+        ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,10 +1,28 @@
 <h3>Fornecedor</h3>
 
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+{{--@if(count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem fornecedores cadastrados.</h3>
 @elseif(count($fornecedores) > 10)
     <h3>Existem vários fornecedores cadastrados.</h3>
 @else
     <h3>Ainda não existem fornecedores cadastrados.</h3>
-@endif 
+@endif --}}
+
+Fornecedor: {{ $fornecedores[1]['nome']}}
+<br>
+Status: {{ $fornecedores[1]['status']}}
+<br>
+@isset($fornecedores[1]['CNPJ'])
+    CNPJ: {{ $fornecedores[1]['CNPJ']}}
+    @empty($fornecedores[1]['CNPJ'])
+        -Vazio
+    @endempty
+@endisset
+<br>
+
+@if($fornecedores[0]['status'] == 'N')
+    <h4>Fornecedor inativo.</h4>
+@else
+    <h4>Fornecedor ativado!</h4>
+@endif
