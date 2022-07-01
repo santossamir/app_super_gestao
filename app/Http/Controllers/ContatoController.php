a613a9306a12ad15258db01f19c14235d40a2122
Samir Santos <santossamir@hotmail.com>
2022-06-30 22:57:04 -0300

    Validando os campos do form e recuperando as irformações dos inputs caso ocorra erro no envio.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,18 +7,25 @@ use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(Request $request){
-       
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-        $contato->save();
-                
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
-
+    public function contato(Request $request){     
         
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);        
+    }
+
+    public function salvar(Request $request){
+        //validar dados do form no request
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+        //SiteContato()::create($request->all());
     }
 }
