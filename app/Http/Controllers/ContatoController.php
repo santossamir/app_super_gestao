367ab955487cf0639f707b3f66f01d3500a9a263
Samir Santos <santossamir@hotmail.com>
2022-06-30 21:18:59 -0300

    Gravando os dados do form no banco de dados.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        /*echo '<pre>';
-        var_dump($_POST);
-        echo '</pre>';*/
-        return view('site.contato');
+    public function contato(Request $request){
+       
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+                
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
+
+        
     }
 }
