6fdffdfb1a8f33d462a2904f90d003151976434b
Samir Santos <santossamir@hotmail.com>
2022-07-16 20:51:33 -0300

    Traduzindo as mensagens de erros para os campos do form.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -16,15 +16,27 @@ class ContatoController extends Controller
     }
 
     public function salvar(Request $request){
+
         //validar dados do form no request
-        $request->validate([
+        $regras = [ 
             'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
-        
+        ];
+
+        $feedback = [  
+            'required' => 'O campo :attribute deve ser preenchido.',
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso.',
+            'email.email' => 'O email informado não é válido.',
+            'mensagem.max' => 'A mensagem deve ter no máxmo 2000 caracteres.'
+        ];
+
+        $request->validate($regras, $feedback);
+
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
